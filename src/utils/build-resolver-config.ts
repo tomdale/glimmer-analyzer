bc10d49157307e0ffe1c4f538c5e4fbbde7a2fd5
Tom Dale <tom@tomdale.net>
2017-10-16 13:19:20 -0400

    Remove @glimmer/application-pipeline dependency

--- src/utils/build-resolver-config.ts
+++ src/utils/build-resolver-config.ts
@@ -1,6 +1,35 @@
 import { ResolverConfiguration } from '@glimmer/resolver';
-import DEFAULT_MODULE_CONFIGURATION from '@glimmer/application-pipeline/dist/lib/broccoli/default-module-configuration';
 import { Config } from './load-config-factory';
+
+const DEFAULT_MODULE_CONFIGURATION = {
+  types: {
+    application: { definitiveCollection: 'main' },
+    component: { definitiveCollection: 'components' },
+    'component-test': { unresolvable: true },
+    helper: { definitiveCollection: 'components' },
+    'helper-test': { unresolvable: true },
+    renderer: { definitiveCollection: 'main' },
+    template: { definitiveCollection: 'components' }
+  },
+  collections: {
+    main: {
+      types: ['application', 'renderer']
+    },
+    components: {
+      group: 'ui',
+      types: ['component', 'component-test', 'template', 'helper', 'helper-test'],
+      defaultType: 'component',
+      privateCollections: ['utils']
+    },
+    styles: {
+      group: 'ui',
+      unresolvable: true
+    },
+    utils: {
+      unresolvable: true
+    }
+  }
+};
 
 export default function buildResolverConfig(config: Config, pkgName: string): ResolverConfiguration {
   let moduleConfig = config.moduleConfiguration || DEFAULT_MODULE_CONFIGURATION;
